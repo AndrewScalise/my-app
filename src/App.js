eb8f46baa141e83094d4956690155487c18dbeaa
Mugen <mugennewjabes@gmail.com>
2023-06-02 02:15:20 -0500

    ui setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Card from "./components/card";
+import shuffle from "./utlities/shuffle"
 
 function App() {
+  const [cards, setCards] = useState(shuffle); // Cards array from assets
+
   return (
-    <div className="App">
-    </div>
+    <>
+      <div className="grid">
+        {cards.map((card) => {
+          const { image, id, matched } = card;
+
+          return (
+            <Card key={id} image={image} selected={false} onClick={() => {}} />
+          );
+        })}
+      </div>
+    </>
   );
 }
 
